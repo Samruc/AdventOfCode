b42e80d805a03479131f17d3260c5e450d672148
Samruc <marcuskardell@hotmail.com>
2024-12-10 19:54:44 +0100

    Add solution for 6A
    Also an incomplete attempt at 6B

--- AOC2024.py
+++ AOC2024.py
@@ -51,3 +51,35 @@ mark("5B", sum(int(sorted(l, key=cmp_to_key(lambda a, b: 1 if b + "|" + a in rul
                 if any(i < j and b + "|" + a in rules
                         for i, a in enumerate(l) for j, b in enumerate(l))
                 else 0 for l in lists), 5169)
+
+data = read("6", 2024, raw=True, strip=True)
+dirs = {"^": (0, -1), ">": (1, 0), "v": (0, 1), "<": (-1, 0)}
+turn = {"^": ">", ">": "v", "v": "<", "<": "^"}
+location = None
+direction = None
+for y, l in enumerate(data):
+    for x, c in enumerate(l):
+        if c in dirs:
+            location = (x, y)
+            direction = c
+visited = {location}
+visited_with_dir = {str(location) + direction}
+loop_blocks = set()
+while True:
+    try:
+        x = location[0] + dirs[direction][0]
+        y = location[1] + dirs[direction][1]
+        if data[y][x] in [".", "^"]:
+            if data[y][x] == ".":
+                if str(location) + turn[direction] in visited_with_dir:
+                    loop_blocks.add((x, y))
+            location = (x, y)
+            visited.add(location)
+            visited_with_dir.add(str(location) + direction)
+        else:
+            direction = turn[direction]
+    except IndexError:
+        break
+
+mark("6A", len(visited), 5269)
+print(len(loop_blocks))  # Not correct, loop test is too simple. Undershoots on test data as well
